35beb05ea9ed0e12064c14e3b8354df44795518b
Eric-Stanford <erics6716@gmail.com>
2021-04-11 19:43:47 -0400

    Added color variability to counties, as well as popups that show the county names. Made small aesthetic changes to the background.

--- frontend/hotspot-map/src/App.js
+++ frontend/hotspot-map/src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import MyMap from "./components/mymap";
+import './App.css'
 
 function App() {
-  return <MyMap />;
+  return (
+    <div>
+        <h1 style = {{textAlign: "Center"}}> Covid-19 Hotspot Map</h1>
+          <MyMap />
+    </div>
+
+  );
 }
 
 export default App;
--- frontend/hotspot-map/src/components/mymap.jsx
+++ frontend/hotspot-map/src/components/mymap.jsx
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import {MapContainer, GeoJSON, TileLayer, Marker} from "react-leaflet"
 import county_ca from './../data/county_ca.json'
 import "leaflet/dist/leaflet.css";
+
+
+
 class MyMap extends Component {
     state = { };
 
@@ -9,12 +12,39 @@ class MyMap extends Component {
         console.log(county_ca);
     }
 
+    getColor = (n) => {
+        if(n > 2){
+            return "red";
+        }
+        else{
+            return "blue";
+        }
+    
+    };
+
+    countyStyle = {
+        fillColor: "blue",
+        fillOpacity: .6,
+        color: "black",
+    };
+
+   
+    onEachCounty = (county, layer) =>{
+        const countyName = county.properties.NAME;
+        layer.bindPopup(`${countyName}`)
+         
+        if(county.properties.COUNTYFP > 34){
+            layer.options.fillColor = "green"
+        }
+
+
+    };
+
     render() {
         return ( 
             <div>
-                <h1 style = {{textAlign: "center"}}>My Map</h1>
-                <MapContainer style = {{height: "80vh"}} zoom = {6.5} center = {[38, -122]}>
-                    <GeoJSON data ={county_ca.features}/>
+                <MapContainer style = {{height: "80vh", width: '80%', margin: 'auto'}} zoom = {6.5} center = {[38, -122]} scrollWheelZoom = "false">
+                    <GeoJSON style = {this.countyStyle} data ={county_ca.features} onEachFeature={this.onEachCounty}/>
                     <TileLayer
                      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
