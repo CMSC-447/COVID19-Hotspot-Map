1ad87fa6affcae2350399c4b63a6bdfecc4fbbcb
Faran Nadeem <70853979+Fnadeem1@users.noreply.github.com>
2021-05-10 15:08:01 -0400

    Update index.js
    added router for the to access the new page

--- frontend/hotspot-map/src/index.js
+++ frontend/hotspot-map/src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from 'react-router-dom';
+import Routing from './router'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter><Routing /></BrowserRouter>
+    
   </React.StrictMode>,
-  document.getElementById('root')
+ 
+
+  document.getElementById('root'),
+ 
 );
 
 // If you want to start measuring performance in your app, pass a function
